51886ecd455793215345917478eb7632f96e0611
Zhe Li <lizhe5@mail.sysu.edu.cn>
2024-01-23 15:17:45 +0800

    pyautomd first commit: include code and example

---
+++ src/rec_lig_com_pdb_generator.py
@@ -0,0 +1,52 @@
+from .nonstandard_residue_preparation.formate_lig_pdb import gen_ligpdb_by_prepi_formated_pdb
+
+class PDB_simple_processor:
+    def __init__(self, prepi_file, ligand_format_pdb, receptor_pdb):
+        self.prepi_file = prepi_file
+        self.ligand_format_pdb = ligand_format_pdb
+        self.receptor_pdb = receptor_pdb
+        self.rec_name_u = receptor_pdb.split(".")[0]
+        with open(self.receptor_pdb, "r") as file:
+            lines = file.readlines()
+        self.watline = next((i for i, line in enumerate(lines, 1) if ("HOH" in line or "WAT" in line) and ("HETATM" in line or "ATOM" in line)), None)
+
+    def generate_lig_pdb(self):
+        print("Generating lig.pdb")
+        gen_ligpdb_by_prepi_formated_pdb(formated_pdb=self.ligand_format_pdb, prepi_file=self.prepi_file, ligpdb='lig.pdb')
+        with open("lig.pdb", "a") as file:
+            file.write("TER\n")
+
+    def generate_rec_pdb(self):
+        print("Generating rec.pdb")
+        with open(self.receptor_pdb, "r") as file:
+            lines = file.readlines()
+        if self.watline:
+            with open("rec.pdb", "w") as file:
+                file.writelines(lines[:self.watline - 1])
+
+    def generate_rec_lig_pdb(self):
+        print("Generating rec-lig.pdb")
+        with open("rec-lig.pdb", "w") as outfile:
+            with open("rec.pdb", "r") as infile:
+                outfile.write(infile.read())
+            with open("lig.pdb", "r") as infile:
+                outfile.write(infile.read())
+
+    def generate_combined_pdb(self):
+        combined_pdb_name = f"{self.rec_name_u}_MOL.pdb"
+        print(f"Generating {combined_pdb_name}")
+        with open(combined_pdb_name, "w") as outfile:
+            with open("rec.pdb", "r") as infile:
+                outfile.write(infile.read())
+            with open("lig.pdb", "r") as infile:
+                outfile.write(infile.read())
+            with open(self.receptor_pdb, "r") as infile:
+                if self.watline:
+                    outfile.writelines(infile.readlines()[self.watline:])
+
+        # Removing lines starting with 'CONECT' and empty lines
+        with open(combined_pdb_name, "r") as file:
+            lines = file.readlines()
+        with open(combined_pdb_name, "w") as file:
+            file.writelines(line for line in lines if not line.startswith("CONECT") and line.strip())
+
---
+++ src/workflow/auto_lig_parm_preparation.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from ..nonstandard_residue_preparation import antechamber_relate_module
+from ..nonstandard_residue_preparation import gaussian_relate_module
+from ..nonstandard_residue_preparation import formate_lig_pdb
+
+
+def main(forcefield_needed, input_lig_pdb, lig_resname, charge_model='bcc', lig_net_charge=0, gaussian_scr_path=None, gaussian_excute=None, ligifopt=False,
+            ):
+    """
+    The workflow of the solvated receptor-ligand system preparation for amber MD simulation.
+
+    :param forcefield_needed: dictioanry, the forcefield needed for the system.
+    :param input_lig_pdb: string, the input ligand pdb file.
+    :param lig_resname: string, the ligand residue name.
+    :param charge_model: string, the charge model, 'bcc' or 'resp'.
+    :param lig_net_charge: int, the net charge of the ligand.
+    :param gaussian_scr_path: string, the path of the gaussian scratch.
+    :param gaussian_excute: string, the excute file of the gaussian.
+    :param ligifopt: bool, whether to optimize the ligand structure.
+    """
+    protein_ff = forcefield_needed['protein_ff']
+    # Generate the ligand parameters
+    small_molecule_ff = forcefield_needed['small_molecule_ff']
+    formate_lig_pdb.format_lig_pdb(input_lig_pdb, lig_resname)# Generate the ligand-format.pdb
+    ifcheck_gaussian_excute = True if charge_model == 'resp' else False
+    gen_gaussian = gaussian_relate_module.Format_pdb_gen_gaussian('ligand-format.pdb', 1, lig_resname, lig_net_charge, protein_ff, gaussian_scr_path, gaussian_excute, ifcheck_gaussian_excute)# Generate the MOL_qm_gaussian.pdb
+    gaussian_lig_pdb = f"{lig_resname}_qm_gaussian.pdb"
+    if charge_model == 'resp':
+        gen_gaussian.create_gaussian_com(ifopt=ligifopt)# Generate the gaussian input file
+        gaussian_input_name = gen_gaussian.gaussian_input_name
+        chk_file = gen_gaussian.chk_file
+        run_gaussian = gaussian_relate_module.Gaussian_run(gaussian_input_name, gaussian_excute, chk_file)
+        run_gaussian.run_gaussian()# Run the gaussian
+        gaussian_out = f"{lig_resname}_qm.log"
+    elif charge_model == 'bcc':
+        gaussian_lig_pdb = f"{lig_resname}_qm_gaussian.pdb"
+        gaussian_out = f"{lig_resname}_qm.log"    
+    prepi_generator = antechamber_relate_module.PrepiGenerator(gaussian_lig_pdb, lig_resname, charge_model, small_molecule_ff.split('.')[-1], gaussian_out)
+    prepi_generator.gen_small_molecule_prepi() 
+    if os.path.exists(f"{lig_resname}.prepi"):
+        if os.path.exists(f"{lig_resname}.frcmod"):
+            print("Ligand parameters successfully generated.")
+    else:
+        print("Ligand parameters generation failed.")
+        sys.exit()
+  
